040db737678e8888a954e2bafb2af810fe81716a
Tim Crisp <timothycrisp@gmail.com>
2022-04-11 21:19:09 -0500

    guitar crud

--- api/views/guitars.py
+++ api/views/guitars.py
@@ -27,3 +27,32 @@ def show(id):
   guitar = Guitar.query.filter_by(id=id).first()
   guitar_data = guitar.serialize()
   return jsonify(guitar=guitar_data), 200
+
+@guitars.route('/<id>', methods=["PUT"])
+@login_required
+def update(id):
+  data = request.get_json()
+  profile = read_token(request)
+  guitar = Guitar.query.filter_by(id=id).first()
+
+  if guitar.profile_id != profile["id"]:
+    return 'Sorry, bubba', 403
+  
+  for key in data:
+    setattr(guitar, key, data[key])
+  
+  db.session.commit()
+  return jsonify(guitar.serialize()), 200
+
+@guitars.route('<id>', methods=["DELETE"])
+@login_required
+def delete(id):
+  profile = read_token(request)
+  guitar = Guitar.query.filter_by(id=id).first()
+
+  if guitar.profile_id != profile["id"]:
+    return 'Get outta town!', 403
+  
+  db.session.delete(guitar)
+  db.session.commit()
+  return jsonify(message="Success"), 200
